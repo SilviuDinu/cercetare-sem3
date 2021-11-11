80539847ee06bba06c35b1f77dfd291f7d30c02f
SilviuDinu <silviu_dinu@yahoo.com>
2021-11-11 22:48:15 +0200

    update video

--- FaceDetectorUtils.py
+++ FaceDetectorUtils.py
@@ -7,7 +7,38 @@ import sys
 import math
 import matplotlib.pyplot as plt
 from PIL import Image
+import torch
+from torchvision import datasets, models, transforms
+import torch.nn as nn
+from torch.nn import functional as F
+import torch.optim as optim
 
+
+emotionsDict = {
+    "Angry": 0,
+    "Disgust": 1,
+    "Fear": 2,
+    "Happy": 3,
+    "Neutral": 4,
+    "Sad": 5,
+    "Surprise": 6,
+}
+
+emotionsDict_3 = {
+    0: "Angry",
+    1: "Happy",
+    2: "Sad"
+}
+
+emotionsDict_7 = {
+     0: "Angry",
+    1: "Disgust",
+    2: "Fear",
+    3: "Happy",
+    4: "Neutral",
+    5: "Sad",
+    6: "Surprise"
+}
 
 
 class FaceDetector():
@@ -46,22 +77,34 @@ class FaceDetector():
     def runVideoDetection(self):
         print('live video detection started')
         cap = cv2.VideoCapture(0)
-
+        model = models.resnet50(pretrained=False)
+        model.fc = nn.Sequential(
+            nn.Linear(2048, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 7))
+        model.load_state_dict(torch.load('models/64acc_resnet50_32epochs_CrossEntroy_3_classes.h5', map_location='cpu'))
+        model.eval()
         while True:
             ret, frame = cap.read()
             try:
                 # detect face box, probability and landmarks
                 boxes, probs, landmarks = self.mtcnn_pytorch.detect(
                     frame, landmarks=True)
+
                 # draw on frame
                 self._draw(frame, boxes, probs, landmarks)
+
 
             except:
                 pass
 
             # Show the frame
             cv2.imshow('Face Detection', frame)
-
+            resizedframe = cv2.resize(frame,(224,224))
+            frame_to_tensor = transforms.ToTensor()(resizedframe)
+            output = model(frame_to_tensor.unsqueeze(0))
+            pred = torch.argmax(output)
+            print(pred, emotionsDict_7[int(pred)])
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -90,8 +133,10 @@ class FaceDetector():
                             x, y, width, height = faces[0]['box']
                             crop_img = pixels[y:y+height, x:x+width]
                             if not os.path.exists(os.path.join(outputPath, emotionCategory)):
-                                os.makedirs(os.path.join(outputPath, emotionCategory))
-                            cv2.imwrite(os.path.join(outputPath, emotionCategory, emotionCategory + '_' + file), crop_img)   
+                                os.makedirs(os.path.join(
+                                    outputPath, emotionCategory))
+                            cv2.imwrite(os.path.join(
+                                outputPath, emotionCategory, emotionCategory + '_' + file), crop_img)
                         except IndexError:
                             pass
                             # create the shape
@@ -112,21 +157,24 @@ class FaceDetector():
                         sys.stdout.write("\bCurrent progress: %s %%\r" %
                                          (str(math.ceil(idx / len(files) * 100))))
 
-                        resnet = InceptionResnetV1(pretrained='vggface2').eval()
+                        resnet = InceptionResnetV1(
+                            pretrained='vggface2').eval()
                         img = cv2.imread(filename)
 
                         mtcnn = self.mtcnn_pytorch
                         # Get cropped and prewhitened image tensor
                         if not os.path.exists(os.path.join(outputPath, emotionCategory)):
-                            os.makedirs(os.path.join(outputPath, emotionCategory))
-                        img_cropped = mtcnn(img, save_path=os.path.join(outputPath, emotionCategory, emotionCategory + '_' + file))
-                            
+                            os.makedirs(os.path.join(
+                                outputPath, emotionCategory))
+                        img_cropped = mtcnn(img, save_path=os.path.join(
+                            outputPath, emotionCategory, emotionCategory + '_' + file))
+
                 except IOError:
                     print("cannot create thumbnail for '%s'" % file)
-        sys.stdout.write("\b\nDone\n")        
-    
+        sys.stdout.write("\b\nDone\n")
+
     def draw_facebox(self, filename, result_list):
-    # load the image
+        # load the image
         data = plt.imread(filename)
         # plot the image
         plt.imshow(data)
@@ -137,12 +185,9 @@ class FaceDetector():
             # get coordinates
             x, y, width, height = result['box']
             # create the shape
-            rect = plt.Rectangle((x, y), width, height, fill=False, color='orange')
+            rect = plt.Rectangle((x, y), width, height,
+                                 fill=False, color='orange')
             # draw the box
             ax.add_patch(rect)
         # show the plot
         plt.show()
-
-
-
-
