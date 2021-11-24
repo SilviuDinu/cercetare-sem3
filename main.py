847135b1f4f54385c11ebff319b7f0e11c2f652f
SilviuDinu <silviu_dinu@yahoo.com>
2021-11-24 14:44:15 +0200

    working picture testing mobilenet

--- main.py
+++ main.py
@@ -51,20 +51,20 @@ def main():
                 transforms.RandomAffine(0, shear=10, scale=(0.8, 1.2)),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
-                normalize
+                # normalize
             ]),
         'validation':
             transforms.Compose([
                 # transforms.Resize((224, 224)),
                 transforms.ToTensor(),
-                normalize
+                # normalize
             ])}
 
     image_datasets = {
         'train':
-            datasets.ImageFolder(os.path.join(curr, r'fer2013_resized/train/'), data_transforms['train']),
+            datasets.ImageFolder(os.path.join(curr, r'fer2013_2/train/'), data_transforms['train']),
         'validation':
-            datasets.ImageFolder(os.path.join(curr, r'fer2013_resized/validation/'), data_transforms['validation'])}
+            datasets.ImageFolder(os.path.join(curr, r'fer2013_2/validation/'), data_transforms['validation'])}
 
     dataloaders = {
         'train':
@@ -86,16 +86,17 @@ def main():
     #model = models.vgg16(pretrained=True).to(device)
     model = models.mobilenet_v2(pretrained=True).to(device)
 
+    # for param in model.parameters():
+    #     # change this to false!!!!!!!!!!!!!!!!!!!
+    #     # This freezes the inner layers
+    #     param.requires_grad = False
 
-    for param in model.parameters():
-        # change this to false!!!!!!!!!!!!!!!!!!!
-        # This freezes the inner layers
-        param.requires_grad = True
 
     model.fc = nn.Sequential(
        nn.Linear(2048, 128),
        nn.ReLU(inplace=True),
-       nn.Linear(128, 7)).to(device)
+       nn.Linear(128, 4)).to(device)
+
     #num_features = model.fc.in_features
     # model.classifier[6] = nn.Sequential(
     #                   nn.Linear(4096, 256), 
@@ -128,7 +129,9 @@ def main():
                     labels = labels.to(device)
 
                     outputs = model(inputs)
+                    # outputs.requires_grad = False
                     loss = criterion(outputs, labels)
+                    # loss.requires_grad = True
 
                     if phase == 'train':
                         optimizer.zero_grad()
@@ -154,30 +157,34 @@ def main():
                                                             epoch_acc))
         return model
     
-    model_trained = train_model(model, criterion, optimizer, num_epochs=16)
+    # model_trained = train_model(model, criterion, optimizer, num_epochs=10)
 
-    if not os.path.exists(os.path.join(curr, r'models/')):
-        os.makedirs(os.path.join(curr, r'models/'))
-    torch.save(model_trained.state_dict(),
-               os.path.join(curr, r'models/mobilenetv2_32epochs_F_nll_loss.h5'))
+    # if not os.path.exists(os.path.join(curr, r'models/')):
+    #     os.makedirs(os.path.join(curr, r'models/'))
+    # torch.save(model_trained.state_dict(),
+    #            os.path.join(curr, r'models/mobilenetv2_32epochs_F_nll_loss.h5'))
 
    # os.system('afplay /System/Library/Sounds/Glass.aiff')
 
 
-    model = models.resnet50(pretrained=False).to(device)
+    model = models.mobilenet_v2(pretrained=False).to(device)
     model.fc = nn.Sequential(
         nn.Linear(2048, 128),
         nn.ReLU(inplace=True),
-        nn.Linear(128, 7)).to(device)
-    model.load_state_dict(torch.load(os.path.join(curr, r'models/mobilenetv2_32epochs_F_nll_loss.h5')))
+        nn.Linear(128, 3)).to(device)
+    model.load_state_dict(torch.load(os.path.join(curr, r'models/Random_Affine_Horizontal_Flip_76_48_acc_mobilenetv2_Angry_Sad_Happy.h5')))
 
     validation_img_paths = []
 
-    for root, dirs, files in os.walk(os.path.join(curr, r'fer2013/validation/')):
+    for root, dirs, files in os.walk(os.path.join(curr, r'Testing/')):
         for idx, file in enumerate(files):
             pathList = os.path.normpath(root).split(os.path.sep)
             emotionCategory = pathList[len(pathList) - 1]
-            filename = os.path.join(input, emotionCategory, file)
+            filename = os.path.join('Testing/', emotionCategory, file)
+            filename_out = os.path.join('Testing/good/', emotionCategory, file)
+            img = Image.open(filename)
+            rgbimg = img.convert('RGB')
+            rgbimg.save(filename_out)
             # filename = emotionCategory + '_' + file
             try:
                 if not file.startswith('.'):
@@ -185,10 +192,17 @@ def main():
                     sys.stdout.write("\bCurrent progress: %s %%\r" %
                                      (str(math.ceil(idx / len(files) * 100))))
                     if (len(validation_img_paths) < 7):
-                        validation_img_paths.append(filename)
+                        validation_img_paths.append(filename_out)
 
             except IOError:
                 pass
+
+    # for img_path in validation_img_paths:
+    #     img = Image.open(img)
+    #     rgbimg = Image.new("RGBA", (48, 48))
+    #     rgbimg.paste(img)
+    #     rgbimg.save('foo.jpg')
+    print(validation_img_paths)
 
     img_list = [Image.open(img_path) for img_path in validation_img_paths]
 
@@ -197,20 +211,17 @@ def main():
 
     pred_logits_tensor = model(validation_batch)
     pred_probs = F.softmax(pred_logits_tensor, dim=1).cpu().data.numpy()
-
+    print(img_list)
     fig, axs = plt.subplots(1, len(img_list), figsize=(20, 5))
     for i, img in enumerate(img_list):
         ax = axs[i]
         ax.axis('off')
-        ax.set_title("{:.0f}% Angry, {:.0f}% Disgust, {:.0f}% Fear, {:.0f}% Happy, {:.0f}% Neutral, {:.0f}% Sad, {:.0f}% Surprise,".format(100*pred_probs[i, 0],
-                                                                                                                                           100*pred_probs[i, 2],
-                                                                                                                                           100*pred_probs[i, 3],
-                                                                                                                                           100*pred_probs[i, 4],
-                                                                                                                                           100*pred_probs[i, 5],
-                                                                                                                                           100*pred_probs[i, 6],
-                                                                                                                                           100*pred_probs[i, 7]))
+        ax.set_title("{:.0f}% Angry, \n{:.0f}% Happy, \n{:.0f}% Sad ".format(100*pred_probs[i, 0],
+                                                                        100*pred_probs[i, 1],
+                                                                        100*pred_probs[i, 2]))
         ax.imshow(img)
 
+    plt.show()
 
 if __name__ == "__main__":
     main()
